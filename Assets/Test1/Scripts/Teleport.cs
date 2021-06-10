033a5252eba9eb80648af1103f8f65745cab75b0
Rodrigo Fontes <fontes.rodrigo14@gmail.com>
2021-06-10 20:07:19 -0300

    Initial commit

---
+++ Assets/Test1/Scripts/Teleport.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Video;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject[] Location;
+    public GameObject MonsterFBX;
+    public Transform monsterPosition;
+    public VideoPlayer dead;
+    NavMeshAgent nav;
+    int i = 0;
+
+    private void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        if(dead.time > 0.9)
+        {
+            Application.Quit();
+        }
+    }
+
+    public void Trasport()
+    {
+        if(i == 0)
+        {
+            nav.SetDestination(Location[i].transform.position);
+        }
+        else if(i == 1)
+        {
+            nav.SetDestination(Location[i].transform.position);
+        }
+        else if(i == 2)
+        {
+            nav.SetDestination(Location[i].transform.position);
+        }
+        i++;
+    }
+    IEnumerator DespawnMonster()
+    {
+        yield return new WaitForSeconds(2f);
+        MonsterFBX.GetComponent<NavMeshAgent>().SetDestination(monsterPosition.position);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        StartCoroutine(DespawnMonster());
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Trigger1"))
+        {
+            StartCoroutine(DespawnMonster());
+        }
+        if (other.gameObject.CompareTag("Trigger2"))
+        {
+            dead.Play();
+            MonsterFBX.GetComponent<NavMeshAgent>().SetDestination(transform.position);
+        } 
+    }
+}
